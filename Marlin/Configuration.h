a1061174f34a2ed0bec466b53abb065c940845c5
bandanam4n <39144281+bandanam4n@users.noreply.github.com>
2019-07-28 21:17:07 -0500

    Flip Around XY to get correct sizing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1034,8 +1034,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 406
-#define Y_BED_SIZE 304
+#define X_BED_SIZE 304
+#define Y_BED_SIZE 406
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
